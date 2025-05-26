7cf335db4123eae8e7df75f938e6207fb3b173a2
PANCHAYUBHANU <89331807+PANCHAYU-BHANU@users.noreply.github.com>
2025-05-26 23:27:57 +0530

    change wining number

--- 4th/script.js
+++ 4th/script.js
@@ -17,13 +17,13 @@ function main() {
     let total = currentTT + x;
     document.getElementById('result').innerHTML = total;
 
-    if(total < 21){
+    if(total < 22){
         document.getElementById('mainResult').innerHTML = "Generate another card?"
-    }else if (total == 21){
+    }else if (total == 22){
         document.getElementById('mainResult').innerHTML = "You are won..!"
         alert('Congratulatins..!');
         document.getElementById('btn-res').style.display = "block"
-    }else if (total>21){
+    }else if (total>22){
         document.getElementById('mainResult').innerHTML = "You are lost..!"
         alert("Lost:(Click restart to play again");
         document.getElementById('btn-res').style.display = "block"
